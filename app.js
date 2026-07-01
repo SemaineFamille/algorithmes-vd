1a4bd103875e24ea1804b9e941d8e81a8fe2b8a2
SemaineFamille <melanie.weil.81@gmail.com>
2026-07-01 15:36:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  * © 2026 – Tous droits réservés
  */
 
-console.log("APP VERSION 01-07-2026 14h30");
+console.log("APP VERSION 01-07-2026 15h30");
 
 let MODE = localStorage.getItem("me") === "true" ? "perso" : "pro";
 // "perso" → STAR
