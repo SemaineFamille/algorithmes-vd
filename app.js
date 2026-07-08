a66d60373211819199989d061cccda3061452325
SemaineFamille <melanie.weil.81@gmail.com>
2026-07-08 10:42:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -765,8 +765,8 @@ const MES_RESUMES = [
  {id: "oxygenation", ordre: 3, titre: "🫁 Oxygénation", chapitre: "Résumés", source: "resume", image: "images/O2_CHAT.png", favori: false },
    {id: "tcc", ordre: 3, titre: "🧠 TCC", chapitre: "Résumés", source: "resume", image: "images/TCC_CHAT.png", favori: false },
   {id: "acr", ordre: 3, titre: "⚰️ ACR", chapitre: "Résumés", source: "resume", image: "images/ACR_CHAT.png", favori: false },
-  {id: "trauma-face", ordre: 4, titre: "👊🏻 Trauma Face", chapitre: "Résumés", source: "resume", image: "images/Trauma face_CHAT.png", favori: false },
-  {id: "trauma-abdo_pelvien", ordre: 4, titre: "Trauma Abdo-pelvien", chapitre: "Résumés", source: "resume", image: "images/Trauma abdo pelvien_CHAT.png", favori: false }
+  {id: "trauma_face", ordre: 4, titre: "👊🏻 Trauma Face", chapitre: "Résumés", source: "resume", image: "images/Trauma face_CHAT.png", favori: false },
+  {id: "trauma_abdo_pelvien", ordre: 4, titre: "Trauma Abdo-pelvien", chapitre: "Résumés", source: "resume", image: "images/Trauma abdo pelvien_CHAT.png", favori: false }
   
 ];
 
